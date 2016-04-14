8aeef108ee1f87d8dad3fdfb2e36c7f6851c6d51
Hugo Gonzalez <github@hugo.labkode.com>
2016-04-14 10:24:45 +0200

    Remove reference to old service-auth to use entities.User

--- mocks/auth.go
+++ mocks/auth.go
@@ -2,7 +2,7 @@ package mocks
 
 import (
 	"github.com/clawio/codes"
-	"github.com/clawio/service-auth/server/spec"
+	"github.com/clawio/entities"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -14,7 +14,11 @@ func (m *MockAuthService) Authenticate(username, password string) (string, *code
 	args := m.Called(username, password)
 	return args.String(0), args.Get(1).(*codes.Response), args.Error(2)
 }
-func (m *MockAuthService) Verify(token string) (*spec.Identity, *codes.Response, error) {
+func (m *MockAuthService) Verify(token string) (entities.User, *codes.Response, error) {
 	args := m.Called(token)
-	return args.Get(0).(*spec.Identity), args.Get(1).(*codes.Response), args.Error(2)
+	return args.Get(0).(entities.User), args.Get(1).(*codes.Response), args.Error(2)
+}
+func (m *MockAuthService) Invalidate(token string) (*codes.Response, error) {
+	args := m.Called(token)
+	return args.Get(0).(*codes.Response), args.Error(1)
 }
