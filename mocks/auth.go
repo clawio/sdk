70064ce92ef0f58a0fa4befd29c262796300582f
Hugo Gonzalez <github@hugo.labkode.com>
2016-04-14 16:15:05 +0200

    Fix golint warnings

--- mocks/auth.go
+++ mocks/auth.go
@@ -6,18 +6,24 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// MockAuthService mocks an AuthService.
 type MockAuthService struct {
 	mock.Mock
 }
 
+// Authenticate mocks the Authenticate call.
 func (m *MockAuthService) Authenticate(username, password string) (string, *codes.Response, error) {
 	args := m.Called(username, password)
 	return args.String(0), args.Get(1).(*codes.Response), args.Error(2)
 }
+
+// Verify mocks the Verify call.
 func (m *MockAuthService) Verify(token string) (entities.User, *codes.Response, error) {
 	args := m.Called(token)
 	return args.Get(0).(entities.User), args.Get(1).(*codes.Response), args.Error(2)
 }
+
+// Invalidate mocks the Invalidate call.
 func (m *MockAuthService) Invalidate(token string) (*codes.Response, error) {
 	args := m.Called(token)
 	return args.Get(0).(*codes.Response), args.Error(1)
